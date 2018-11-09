f782aa3c9b512353d4c72364c993cd7e67f13b51
pluk1 <9225534@gmail.com>
2018-11-09 20:27:47 +0200

    first commit

---
+++ src/main/java/telran/java23/serviceprivder/ServicePrivderApplication.java
@@ -0,0 +1,12 @@
+package telran.java23.serviceprivder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePrivderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePrivderApplication.class, args);
+    }
+}
---
+++ src/test/java/telran/java23/serviceprivder/ServicePrivderApplicationTests.java
@@ -0,0 +1,16 @@
+package telran.java23.serviceprivder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicePrivderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
